24b7492fce0ec110b76a82199ccfdc471c775e00
Choi BoKeun <cbk121779@gmail.com>
2023-10-24 14:18:50 +0900

    6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   // state가 변화할때 모든 component는 다시 실행된다
   // 가끔 component의 내부 몇몇 코드는
   // 처음 한번만 실행되고 다시는 실행되지 않도록 하고 싶을 때
-  //
+  // useEffect를 사용한다
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("call an api");
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("CALL THE API ...");
+  }, []);
 
   return (
     <div>
