90efcc070221a0feb77fc45257d164b0ea31be0f
Choi BoKeun <cbk121779@gmail.com>
2023-10-26 22:05:03 +0900

    7.3 movie app part One

--- src/App.js
+++ src/App.js
@@ -2,45 +2,37 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [usd, setUsd] = useState([]);
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        setCoins(json);
-        setLoading(false);
-      });
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(async () => {
+    getMovies();
   }, []);
-
-  const onChange = (event) => setUsd(event.target.value);
 
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      <input
-        onChange={onChange}
-        value={usd}
-        type="number"
-        placeholder="USD 입력"
-      />
-      USD
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
         <div>
-          <select>
-            {/*map을 사용하면 react.js는 element에 key를 주도록 한다.*/}
-            {coins.map((coin) => (
-              <option key={coin.id}>
-                {coin.name} ({coin.symbol}): {usd / coin.quotes.USD.price}
-                {coin.symbol}
-              </option>
-            ))}
-          </select>
-          <br />
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <h2>{movie.title}</h2>
+              <img src={movie.medium_cover_image} />
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((genre) => (
+                  <li key={genre}>{genre}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       )}
     </div>
