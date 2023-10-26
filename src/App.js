9fd7f1e254c7e848bab0d93cd524f099f6b08056
Choi BoKeun <cbk121779@gmail.com>
2023-10-27 01:27:21 +0900

    7.5 react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
+// react router v6 문법을 따름
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
+
+/*
+//최신버전 v6.17.0
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/movie",
+    element: <Detail />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
+*/
