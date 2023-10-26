1ea8e29db2e2cc635ec0df42cfd6656724742303
Choi BoKeun <cbk121779@gmail.com>
2023-10-26 21:47:33 +0900

    7.2 coin tracker

--- src/App.js
+++ src/App.js
@@ -1,41 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    // 절대 이 안에서는
-    // toDo = "lalala" 이런식으로 하면 안됨
-    // setToDo("lalala") 이렇게 해야함. 항상 수정하는 함수를 쓴다.
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [usd, setUsd] = useState([]);
 
-    // list 업데이트 하는법
-    setToDos((currentArray) => [...currentArray, toDo]);
-    setToDo("");
-  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+
+  const onChange = (event) => setUsd(event.target.value);
+
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      {/* array 안의 값 : item. 이름은 자기 마음대로
-        index는 각각의 아이템의 index. li의 key로 쓰임. 안쓰면 경고 발생.
-      */}
-      {toDos.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      <input
+        onChange={onChange}
+        value={usd}
+        type="number"
+        placeholder="USD 입력"
+      />
+      USD
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <select>
+            {/*map을 사용하면 react.js는 element에 key를 주도록 한다.*/}
+            {coins.map((coin) => (
+              <option key={coin.id}>
+                {coin.name} ({coin.symbol}): {usd / coin.quotes.USD.price}
+                {coin.symbol}
+              </option>
+            ))}
+          </select>
+          <br />
+        </div>
+      )}
     </div>
   );
 }
